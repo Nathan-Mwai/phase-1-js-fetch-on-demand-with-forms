5b6f58443f6b2ef31a8f596112cd8f5fd6d0e0c3
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-07-10 13:14:59 +0300

    Functions works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector(`form`);
+  inputForm.addEventListener(`submit`, (event) => {
+    event.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+
+    // Access input value using event object
+    // console.log(event.target.children[1].value);
+
+
+    // Access input value directly
+    const input = document.querySelector(`input#searchByID`);
+    console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(data => {
+        // console.log(data)
+        const title = document.querySelector(`section#movieDetails h4`)
+        const summary = document.querySelector(`section#movieDetails p`)
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+    // console.error(`Item does not exist`);
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
